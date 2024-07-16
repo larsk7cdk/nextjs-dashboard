ffd2ed997da936eeb81ea19f4d1b350393ab6e4f
JB6504 <lal@jyskebank.dk>
2024-07-16 09:25:58 +0200

    done chapter 10

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+function Loading() {
+    return (
+        <div>
+            <DashboardSkeleton/>;
+        </div>
+    );
+}
+
+export default Loading;
